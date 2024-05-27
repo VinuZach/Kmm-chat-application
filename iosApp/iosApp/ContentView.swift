ddf162741ca75e246b37272f9fbaf98710efbbda
vinu <mathewzchrh603@gmail.com>
2024-05-27 15:59:19 +0530

    changes for api call in IOS

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -9,25 +9,52 @@ struct ContentView: View {
         {
             
             
-            TextField("Username",text: $viewModel.username)  .padding(10)
-                .overlay(RoundedRectangle(cornerRadius: 4).stroke(Color.gray, lineWidth: 2))
-            
-            TextField("Password",text: $viewModel.password).padding(10)
-                .overlay(RoundedRectangle(cornerRadius: 4).stroke(Color.gray, lineWidth: 2))
+//            TextField("Username",text: $viewModel.username)  .padding(10)
+//                .overlay(RoundedRectangle(cornerRadius: 4).stroke(Color.gray, lineWidth: 2))
+//            
+//            TextField("Password",text: $viewModel.password).padding(10)
+//                .overlay(RoundedRectangle(cornerRadius: 4).stroke(Color.gray, lineWidth: 2))
 
             
             Text("Forgot Password").font(.custom("reset passord", size: 12)).frame(maxWidth: .infinity,alignment:.trailing)
-        
+           
             Button(action: {
-               
-               
+                var inst: GGGGOnDOne{
+                    class FFF:GGGGOnDOne
+                    {
+                        func aaaa() {
+                          print("ffff")
+                        }
+                    }
+                    return FFF()
+                }
+//                GGGG().cakk(a: "12", b: "12", c: 12, d: inst)
+//                exampleFunction(param1: "asdasd", param2: "asdsa", ourFunc: { (no1, no2) -> Int in
+//                    return no1 + no2
+//                  })
+                GGGG().aaa(c: {(a,d) -> Void in
+                    print(a)
+                    print(d)
+                    if(a) as! Bool
+                    {
+                        print("asdasd")
+                    }
+                })
+                ApiHandler(apiCallManager: Greeting().getHttpClientForApi1()).verifyUserDetails(userName: "aaa@aaa.com", password: "aaa", onResultObtained: {(a,b) in
+                print(a)
+                    print(b)
+                }, completionHandler: {err in
+                    print(err)
+                })
             }, label: {
-                Text("Submit").padding(.vertical,10).frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                Text("submit").padding(.vertical,10).frame(maxWidth: 12   )
             }).buttonStyle(.borderedProminent).padding(.top,40)
             
         }.padding(20)
        
     }
+ 
+  
 }
 
 
@@ -40,7 +67,7 @@ extension ContentView {
         @Published var username = ""
         @Published var password = ""
         init() {
-        
+           
         }
     }
 }
