b9a906ae0b5c639e68586a13721fbc9098d6b706
vinu <mathewzchrh603@gmail.com>
2024-05-30 08:29:31 +0530

    drag and drop

--- androidApp/src/main/java/com/example/chatapplication/android/chat/ChatGroupsAndListing.kt
+++ androidApp/src/main/java/com/example/chatapplication/android/chat/ChatGroupsAndListing.kt
@@ -1,19 +1,22 @@
 package com.example.chatapplication.android.chat
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.draganddrop.dragAndDropSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,36 +25,85 @@ import androidx.compose.ui.unit.sp
 @Preview
 @Composable
 fun ChatGroupAndListingMain() {
-    Column() {
+    LongPressDraggable(modifier = Modifier.fillMaxSize()) {
+        Column() {
 
 
-        LazyRow(modifier = Modifier.fillMaxWidth().background(Color.White), horizontalArrangement = Arrangement.Center) {
-            items(count = 2) {
-                GroupItemDetail(groupName = "group name $it")
+            LazyRow(modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White), horizontalArrangement = Arrangement.Center) {
+                items(count = 2) {
+                    GroupItemDetail(groupName = "group name $it")
+                }
+            }
+            LazyColumn(modifier = Modifier
+                    .padding(top = 10.dp)
+                    .background(Color.LightGray)) {
+                items(count = 10)
+                {
+                    ChatItemDetails(chatTitle = "chat item $it")
+                }
             }
         }
-        LazyColumn(modifier = Modifier.padding(top = 10.dp).background(Color.LightGray )) {
-            items(count = 10)
-            {
-                ChatItemDetails(chatTitle = "chat item $it")
-            }
-        }
-    }
+}
+
 }
 @Preview
 @Composable
 fun GroupItemDetail(groupName: String = "sample Group ") {
-    Card(modifier = Modifier.background(Color.White).padding(2.dp), shape = RoundedCornerShape(12)) {
-        Text(text = groupName, modifier = Modifier.padding(10.dp))
+    val chatToGroupList = remember {
+        mutableStateMapOf<String, String>()
+    }
+    DropTarget<String>(
+        modifier = Modifier
+                .padding(6.dp)
+
+    ) { isInBound, foodItem ->
+        val bgColor = if (isInBound) Color.Red else  Color.White
+
+        foodItem?.let {
+            if (isInBound)
+                chatToGroupList[foodItem] = foodItem
+        }
+
+        Column(
+            modifier = Modifier.background( bgColor, RoundedCornerShape(16.dp)),
+        ) {
+            // ...User Image and name Text views
+
+            if (chatToGroupList.isNotEmpty()) {
+                Text(
+                    text = groupName,
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(
+                    text = "${chatToGroupList.size} Items",
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+            }
+            else
+                Text(
+                    text = groupName,
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.ExtraBold
+                )
+        }
     }
 }
 
 @Preview
 @Composable
 fun ChatItemDetails(chatTitle: String = "sample chat item") {
-    Text(text = chatTitle, modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(), fontSize = 20.sp)
+    Row(Modifier.fillMaxWidth().background(Color.Transparent).padding(10.dp)) {
+        DragTarget(modifier = Modifier.fillMaxWidth(), dataToDrop = chatTitle) {
+            Text(text = chatTitle)
+        }
+    }
+
 }
 
 
