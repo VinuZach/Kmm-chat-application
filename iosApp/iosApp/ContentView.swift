59226c80164edf29cd07d8c46c4465c241199a74
vinu <mathewzchrh603@gmail.com>
2024-06-24 09:20:55 +0530

    assign room to group ios changes

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,15 +1,16 @@
 import SwiftUI
-import SimpleToast
 import shared
+import UniformTypeIdentifiers
+
+extension UTType
+{
+    static let chatItemDetails = UTType(exportedAs: "orgIdentifier.iosApp")
+}
+
 struct ContentView: View {
     @ObservedObject private(set) var viewModel: ViewModel
-    
-    private let toastOptions = SimpleToastOptions(alignment: .top,
-                                                  hideAfter: 2,
-                                                  
-                                                  animation: .default,
-                                                  modifierType: .slide
-    )
+    @State var showDialog:Bool=false
+ 
     
     var body: some View {
         
@@ -60,9 +61,11 @@ struct ContentView: View {
     }
     
   
+    
     var groupListingView:some View
     {
      
+       
         VStack
         {
             let groupDetailsList = viewModel.groupListingWithChat?.clusterRoomGroups ?? [ChatRoomWithTotalMessage]()
@@ -72,16 +75,61 @@ struct ContentView: View {
                      ForEach(groupDetailsList,id: \.clusterGroupId)
                     {
                         groupDetails in
-                        Text(groupDetails.roomName).padding(10)
+                        Text(groupDetails.roomName)
                             .overlay(RoundedRectangle(cornerRadius: 5)
                                         .stroke(.gray, lineWidth: 1))
                             .onTapGesture {
                                 viewModel.retrieveGroupDetails(groupDetails: groupDetails)
                             }
+                            .dropDestination(for:ChatItemDetails.self){ droppedRoomDetailList,location in
+                          
+                                viewModel.reassignRoomToGroup=ReassignRoomToGroup(selectedRoom: droppedRoomDetailList.first!, selectedGroup: groupDetails)
+                                print(groupDetails)
+                                showDialog=true
+                                return false
+                                
+                            }.alert(isPresented: $showDialog, content: {
+                               
+                                var selectedRoom=viewModel.reassignRoomToGroup!.selectedRoom
+                                var selectedGroup=viewModel.reassignRoomToGroup!.selectedGroup
+                                let title = if((selectedRoom.clusterGroupId) != "None")
+                                {
+                                    "Reassign to "+selectedGroup.roomName
+                                } else
+                                {
+                                    "Assign room to "+selectedGroup.roomName
+                                }
+                                
+                                let message = if((selectedRoom.clusterGroupId) != "None")
+                                {
+                                    if(selectedGroup.roomCountUnderGroup-1 == 0)
+                                    {
+                                        "Group will be deleted ..Reassign room?"
+                                    }
+                                    else
+                                    {
+                                        "Reassign group?"
+                                    }
+                                } else
+                                {
+                                    "Assign room to group"
+                                }
+                                return Alert(title: Text(title),
+                                             message: Text(message),
+                                             primaryButton: .default(Text("Confirm"))
+                                             {
+                                  
+                                    viewModel.assignRoomToSelectedGroup(groupID: selectedGroup.clusterGroupId!,
+                                                                        roomId: Int32(selectedRoom.roomId))
+                                            viewModel.reassignRoomToGroup=nil
+                                            },
+                                             secondaryButton: .cancel())
+                            })
                     }
 
                      .listStyle(.plain)
                 }
+              
             }.frame(maxWidth:.infinity)
                 .background(.red)
             let chatMessageList = viewModel.groupListingWithChat?.chatRoomWithTotalMessage ??
@@ -105,10 +153,14 @@ struct ContentView: View {
             }
             List(chatMessageList,id: \.roomID) {
                 chatMessageDetails in
+                let chatDetails = ChatItemDetails(roomId: chatMessageDetails.roomID as! Int, roomName: chatMessageDetails.roomName, clusterGroupId: chatMessageDetails.clusterGroupId, totalMessage: chatMessageDetails.totalMessages, roomCountUnderGroup: chatMessageDetails.roomCountUnderGroup)
+                
                 Text(chatMessageDetails.roomName)
+                   
                     .onTapGesture {
                         viewModel.navigationPath.append(chatMessageDetails)
-                    }
+                    }.draggable(chatDetails)
+                    
             }
             
         }
@@ -124,6 +176,30 @@ struct ContentView: View {
     ContentView(viewModel: ContentView.ViewModel())
 }
 
+struct ReassignRoomToGroup
+{
+    var selectedRoom:ChatItemDetails
+    var selectedGroup:ChatRoomWithTotalMessage
+    
+    init(selectedRoom: ChatItemDetails, selectedGroup: ChatRoomWithTotalMessage) {
+        self.selectedRoom = selectedRoom
+        self.selectedGroup = selectedGroup
+    }
+}
+
+struct ChatItemDetails:Codable,Transferable{
+    let roomId:Int
+    let roomName:String
+    let clusterGroupId:String?
+    let totalMessage:Int32
+    let roomCountUnderGroup:Int32
+    
+    static var transferRepresentation: some TransferRepresentation
+    {
+        CodableRepresentation(contentType: .chatItemDetails)
+    }
+
+}
 
 extension ContentView {
     class ViewModel: ObservableObject {
@@ -134,8 +210,25 @@ extension ContentView {
         @Published var navigationPath = [ChatRoomWithTotalMessage]()
         var selectedRoomGroup: ChatRoomWithTotalMessage? = nil
         var webSocket = Greeting().provideChatSocketService()
+     
+        var reassignRoomToGroup:ReassignRoomToGroup?=nil
+        
+        
       
         
+        func assignRoomToSelectedGroup(groupID:String,roomId:Int32) {
+            DispatchQueue.main.async {
+                ApiHandler(apiCallManager: Greeting().getHttpClientForApi1())
+                    .assignRoomToSelectedGroup(roomId: roomId, groupId: Int32(groupID)!, userOverride: true){
+                        (isSuccess, result) in
+                        print(isSuccess)
+                        print(result)
+                    }
+                    completionHandler: { err in
+                    print("cccc")
+                }
+            }
+        }
         func initGroupListingSocketConnection()  {
             DispatchQueue.main.async {
                 self.initSocketConnection(webSocketLink: "/chatList", isForChat: false){
@@ -227,5 +320,7 @@ extension ContentView {
 
             }
         }
+    
     }
+   
 }
